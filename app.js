53499fcd609e51fd44d92a53dadbc1c6e897d554
fikri aulia <fikriaulia@fikris-MacBook-Pro.local>
2021-05-11 12:34:05 +0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+
+app.use(
+  express.urlencoded({
+    extended: true
+  })
+)
+
+app.use(express.json())
+
+const port = 3000
+
+app.post('/', (req, res) => {
+  	res.send('Success!!')
+	console.log(Date.now())
+	console.log(req.body)
+})
+
+app.listen(port, () => {
+  	console.log(`Example app listening at http://localhost:${port}`)
+})
